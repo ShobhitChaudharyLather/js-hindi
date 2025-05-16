735c101d95bae8b0894b72979b623f6d8cc0ea41
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2025-05-16 15:08:03 +0000

    variables

---
+++ 1_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=1245
+let accountEmail="shobhitlather@gmail.com"
+var accountPassword="12345"
+accountCity="Shamli"
+
+// accountId=2 not allowed
+accountEmail="sh@gm.com"
+accountPassword="2354"
+accountCity="Noida"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
