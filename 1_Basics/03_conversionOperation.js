1a94a926fec641d17710cfa1049865e467850fea
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2025-05-16 15:28:33 +0000

    conversion of dataTypes

---
+++ 1_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+//________Conversion______________
+let score="33abc"
+// console.log(typeof score)
+let valInNumber= Number(score)
+// console.log(typeof valInNumber)
+// console.log(valInNumber)
+
+/* 
+    "33" -> 33
+    "33abc"-> NaN
+    true-> 1
+    false-> 0
+*/
+
+let isLoggedIn= 1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/* 1-> true
+   ""-> false
+   "shobhit"->true
+*/
+
+let num=33
+let stringNum=String(num)
+console.log(stringNum)
+console.log(typeof stringNum)
+
+//__________________Operations_______________
