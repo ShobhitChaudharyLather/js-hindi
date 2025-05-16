bd60e3294b12fd86575d9256cd630d0043b650d1
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2025-05-16 16:10:19 +0000

    comparisons

---
+++ 1_Basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log("2">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+/* comparisions convert null to a number, treating it as 0.
+that's why null>=0 is true , null>0 is false
+*/
+ 
+//=== strict check (compare data types as well as value)
+console.log("2"===2);
