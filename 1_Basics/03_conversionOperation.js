4de1b68070cc9526ae94ad284944aac60259b398
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2025-05-16 16:00:19 +0000

    conversion operations

--- 1_Basics/03_conversionOperation.js
+++ 1_Basics/03_conversionOperation.js
@@ -14,7 +14,7 @@ let valInNumber= Number(score)
 
 let isLoggedIn= 1
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 /* 1-> true
    ""-> false
@@ -23,7 +23,28 @@ console.log(booleanIsLoggedIn)
 
 let num=33
 let stringNum=String(num)
-console.log(stringNum)
-console.log(typeof stringNum)
+// console.log(stringNum)
+// console.log(typeof stringNum)
 
 //__________________Operations_______________
+let val= 3;
+let negVal= -val;
+// console.log(negVal);
+
+let str1="Hello";
+let str2=" shobhit";
+let str3=str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1" );
+
+let x=3;
+const y=x++;
+console.log(`x:${x}, y:${y}`);
+
+let a=3;
+const b=++a;
+console.log(`a:${a}, b:${b}`);
