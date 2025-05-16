617e6e6ac26d123a9bec2ef5f5aadd198da4f6e1
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2025-05-16 17:25:42 +0000

    strings

---
+++ 1_Basics/05_strings.js
@@ -0,0 +1,29 @@
+const name = "shobhit-chaudhary-lather"
+const repoCount= 2
+// console.log(name + repoCount + "value")
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+const name2= new String('shobhit-chaudhary')
+// console.log(name2)
+// console.log(name2[0])
+// console.log(name2.__proto__)
+// console.log(name2.length)
+// console.log(name2.toUpperCase())
+// console.log(name2.charAt(2))
+// console.log(name2.indexOf('t'))
+
+const newStr=name.substring(0,4)
+// console.log(newStr);
+
+let anotherStr=name.slice(-2, 4)
+console.log(anotherStr)
+
+const newStr1="   shobhit   "
+console.log(newStr1)
+console.log(newStr1.trim())
+
+const bro="shobhit"
+console.log(bro.replace('shob','ro'))
+console.log(bro.includes('abhi'))
+
+console.log(name.split('-'))
