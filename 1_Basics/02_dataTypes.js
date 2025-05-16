44314e0ff1e4afd11f5da1a3b36a3c6e74661af8
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2025-05-16 15:08:24 +0000

    data types

---
+++ 1_Basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+// alert("Hi!!!") can't use in nodejs
+ let name="Shobhit"
+ let age=22
+ let isMarried=false
+
+ /* Data Types- 
+ number , bigInt, string, boolean, null, undefined, symbol(unique) */
+
+ console.log(typeof name)
+ console.log(typeof age)
+ console.log(typeof isMarried)
+ console.log(typeof null)
+ console.log(typeof undefined)
