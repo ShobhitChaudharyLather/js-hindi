6a2914bddf24f5344754b97ff5e298b34736097b
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2025-05-19 14:53:54 +0000

    object destructure intro

---
+++ 2_Basics/04_objectsDestructure.js
@@ -0,0 +1,25 @@
+// object de-structure
+
+const course ={
+    coursename:"JS",
+    price:"999",
+    courseInstructor:"Hitesh Choudhary"
+}
+
+// course.courseInstructor
+
+const {courseInstructor : instructor}= course 
+console.log(instructor);
+
+//json
+// {
+//     name:"Shobhit",
+//     "cousename":"JS",
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
