7f6d47ed1676e8255795d38282f21b4abcbf35e6
Shobhit Kumar <148740031+ShobhitChaudharyLather@users.noreply.github.com>
2025-05-22 12:40:55 +0000

    while & do-while loops

---
+++ 5_Iterations/two.js
@@ -0,0 +1,23 @@
+//while loop
+
+let i=0
+while (i<=10) {
+    // console.log(`Value of index is ${i}`);
+    i+=2
+}
+
+let arr=['rohan','abhi','saksham','atharva']
+
+let j=0
+while (j < arr.length) {
+    // console.log(arr[j]);
+    j++
+}
+
+//do while loop
+
+let score=11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
